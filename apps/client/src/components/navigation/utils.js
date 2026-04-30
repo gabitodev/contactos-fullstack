1702f486c53abf9b8cc4bb5e7056866ace3ce9d4
Gabriel Garcia <gabitodev@gmail.com>
2026-04-30 11:43:55 -0400

    fixed nav bug

--- apps/client/src/components/navigation/utils.js
+++ apps/client/src/components/navigation/utils.js
@@ -2,13 +2,16 @@ import authService from '../../features/auth/auth.service';
 
 export const getLinks = (pathname) => {
   let links = [];
+  console.log('112121221221');
 
-  if (pathname === '/login') {
+  console.log(pathname);
+
+  if (pathname === '/login/') {
     links = links.concat({ name: 'Registro', to: '/signup' });
     links = links.concat({ name: 'Inicio', to: '/' });
   }
 
-  if (pathname === '/signup') {
+  if (pathname === '/signup/') {
     links = links.concat({ name: 'Inicio', to: '/' });
     links = links.concat({ name: 'Login', to: '/login' });
   }
@@ -24,7 +27,7 @@ export const getLinks = (pathname) => {
 export const getButtons = (pathname) => {
   let buttons = [];
 
-  if (pathname === '/contacts') {
+  if (pathname === '/contacts/') {
     buttons = buttons.concat({
       name: 'Cerrar sesión',
       handler: () => {
