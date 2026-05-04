55b4f25f7e7ae691ffd6d392e702e479c20026ea
Gabriel Garcia <gabitodev@gmail.com>
2026-05-04 11:25:32 -0400

    new service

--- apps/api/services/nodemailer.js
+++ apps/api/services/nodemailer.js
@@ -5,9 +5,7 @@ import nodemailer from 'nodemailer';
  * @type {import('nodemailer').Transporter}
  */
 const nodemailerService = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
